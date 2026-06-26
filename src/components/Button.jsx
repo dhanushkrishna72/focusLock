5311fa157e39f94c6ab9403ad3f67039c7f0e89d
dhanush K <dhanushkrishna2402@icloud.com>
2026-06-26 23:10:12 +0530

    modified the content.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import AndroidIcon from "@mui/icons-material/Android";
 const Button = () => {
   return (
     <div className="button">
-      <a href="sampleCuits/public/app-release.apk" download="FocusLock.apk">
+      <a href="./public/app-release.apk" download="FocusLock.apk">
         <AndroidIcon />
         <h1> Download</h1>
       </a>
